c4a7b38abd5e04e83460c54384c816d6ca63bc54
roman <roman@LAPTOP-9929T6KS>
2022-09-07 22:41:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] surnames = new string[3];
+            string[] names = new string[3];
+            int[] age_years = new int[3];
+            int[] age_months = new int[3];
+            int[] age_sec = new int[3];
+            for (int i = 0; i<3; i++)
+            {
+                Console.Write("Введите свою фамилию: ");
+                surnames[i] = Console.ReadLine();
+                Console.Write("Введите свое имя: ");
+                names[i] = Console.ReadLine();
+                Console.Write("Введите возраст в годах: ");
+                age_years[i] = Convert.ToInt32(Console.ReadLine());
+                if (age_years[i]<0)
+                {
+                    while (age_years[i]<0)
+                    {
+                        Console.Write("Введен некорректный возраст, введите допустимое значение: ");
+                        age_years[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                Console.Write("Введите возраст в месяцах: ");
+                age_months[i] = Convert.ToInt32(Console.ReadLine());
+                if (age_months[i]<1 | age_months[i]>12)
+                {
+                    while (age_months[i]<1 | age_months[i]>12)
+                    {
+                        Console.Write("Введен некорректный возраст в месяцах, введите допустимое значение: ");
+                        age_months[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                age_sec[i] = (age_years[i] * 365 * 24 * 60 * 60) + (age_months[i] * 30 * 24 * 60 * 60);
+                Console.WriteLine("                ");
+            }
+            Console.WriteLine("        ");
+            for (int j = 0; j<3; j++)
+            {
+                Console.WriteLine($"{surnames[j],15} {names[j],15} {age_sec[j],20}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Прямая последовательность действий/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Прямая_последовательность_действий
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свою фамилию: ");
+            string surname1 = Console.ReadLine();
+            Console.WriteLine("Введите свое имя: ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Введите возраст в годах: ");
+            int age_years1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите возраст в месяцах: ");
+            int age_months1 = Convert.ToInt32(Console.ReadLine());
+            int age_sec1 = (age_months1 * 30 * 24 * 60 * 60) + (age_years1 * 365 * 24 * 60 * 60);
+
+
+            Console.WriteLine("Введите свою фамилию: ");
+            string surname2 = Console.ReadLine();
+            Console.WriteLine("Введите свое имя: ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Введите возраст в годах: ");
+            int age_years2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите возраст в месяцах: ");
+            int age_months2 = Convert.ToInt32(Console.ReadLine());
+            int age_sec2 = (age_months2 * 30 * 24 * 60 * 60) + (age_years2 * 365 * 24 * 60 * 60);
+
+            Console.WriteLine("Введите свою фамилию: ");
+            string surname3 = Console.ReadLine();
+            Console.WriteLine("Введите свое имя: ");
+            string name3 = Console.ReadLine();
+            Console.WriteLine("Введите возраст в годах: ");
+            int age_years3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите возраст в месяцах: ");
+            int age_months3 = Convert.ToInt32(Console.ReadLine());
+            int age_sec3 = (age_months3 * 30 * 24 * 60 * 60) + (age_years3 * 365 * 24 * 60 * 60);
+            Console.WriteLine($"{surname1,15} {name1,15} {age_sec1,20}");
+            Console.WriteLine($"{surname2,15} {name2,15} {age_sec2,20}");
+            Console.WriteLine($"{surname3,15} {name3,15} {age_sec3,20}");
+            Console.ReadKey();
+
+        }
+    }
+}
